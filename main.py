9ed43a9fe9763d1047623157265b42260a46a319
Jeff Leinen <jeff.leinen@tyson.com>
2018-10-17 18:02:57 +0000

    Changed label to Jeff

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello Devin Graham!')
+        self.response.write('Hello Jeff!')
 
 
 app = webapp2.WSGIApplication([
